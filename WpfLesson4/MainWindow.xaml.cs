edb3fa713a9c7018863536f8e547a879e7bc0d09
Camalzade Elvin <e.camalzade@itstep.edu.az>
2021-04-11 10:13:49 +0400

    Add project files.

---
+++ WpfLesson4/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfLesson4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<Car> GetCars()
+        {
+            return new List<Car>
+            {
+                new Car
+                {
+                    Vendor="Mercedes",
+                    Model="E-200",
+                    Year=2011
+                },
+                new Car
+                {
+                    Vendor="Ferrari",
+                    Model="F12 Berlinetta",
+                    Year=2011
+                },
+                new Car
+                {
+                    Vendor="BMW",
+                    Model="F10",
+                    Year=2011
+                },
+                new Car
+                {
+                    Vendor="Cadillac",
+                    Model="Escalade",
+                    Year=2011
+                },
+                new Car
+                {
+                    Vendor="Chevrolet",
+                    Model="Cruze LTZ RS",
+                    Year=2015
+                },
+            };
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            listBox.ItemsSource = GetCars();
+
+        }
+
+        private void downBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //if (prgbar.Value > prgbar.Minimum)
+            //{
+            //    prgbar.Value--;
+            //}
+        }
+
+        private void upBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //if (prgbar.Value < prgbar.Maximum)
+            //{
+            //    prgbar.Value++;
+            //}
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+        //    txtBlock1.Text = Convert.ToInt32(slider.Value).ToString();
+        //    txtBlock2.Text = Convert.ToInt32(slider.Value).ToString();
+        }
+    }
+}
